716af02f7a8059139cfd92798d9ebdcaa242579a
thathillguy <rhill352@gmail.com>
2022-01-11 22:10:59 -0500

    added top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {Toolbar} from "./component/toolbar.component";
+import {HeaderComponent} from "./component/header/header.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from '@angular/material/input';
 import {SearchbarComponent} from "./component/searchbar/searchbar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    Toolbar,
+    HeaderComponent,
     SearchbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/toolbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.less']
-})
-export class Toolbar {
-}
